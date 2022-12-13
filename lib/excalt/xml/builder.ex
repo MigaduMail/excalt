fb54c54c1a04802b2d6af92155ad38ee8e7f05c1
Michael Bruderer <ojos@gmx.ch>
2022-12-13 09:38:11 -0300

    Build the xml for the list of events and todos

--- lib/excalt/xml/builder.ex
+++ lib/excalt/xml/builder.ex
@@ -22,4 +22,73 @@ defmodule Excalt.XML.Builder do
 
     Saxy.encode!(el, [])
   end
+
+  def event_list(from, to) do
+    el =
+      Saxy.XML.element(
+        # "C:calendar-query",
+        "D:propfind",
+        [
+          "xmlns:D": "DAV:",
+          "xmlns:C": "urn:ietf:params:xml:ns:caldav"
+        ],
+        [
+          Saxy.XML.element(
+            "D:prop",
+            [],
+            [
+              Saxy.XML.element("D:getetag", [], ""),
+              Saxy.XML.element("C:calendar-data", [], "")
+            ]
+          ),
+          Saxy.XML.element("C:filter", [], [
+            Saxy.XML.element("C:comp-filter", [name: "VCALENDAR"], [
+              Saxy.XML.element("C:comp-filter", [name: "VEVENT"], [
+                Saxy.XML.element(
+                  "C:time-range",
+                  [start: format_datetime(from), end: format_datetime(to)],
+                  ""
+                )
+              ])
+            ])
+          ])
+        ]
+      )
+
+    Saxy.encode!(el, [])
+  end
+
+  def todo_list do
+    el =
+      Saxy.XML.element(
+        "C:calendar-query",
+        [
+          "xmlns:D": "DAV:",
+          "xmlns:C": "urn:ietf:params:xml:ns:caldav"
+        ],
+        [
+          Saxy.XML.element(
+            "D:prop",
+            [],
+            [
+              Saxy.XML.element("D:getetag", [], ""),
+              Saxy.XML.element("C:calendar-data", [], "")
+            ]
+          ),
+          Saxy.XML.element("C:filter", [], [
+            Saxy.XML.element("C:comp-filter", [name: "VCALENDAR"], [
+              Saxy.XML.element("C:comp-filter", [name: "VTODO"], [])
+            ])
+          ])
+        ]
+      )
+
+    Saxy.encode!(el, [])
+  end
+
+  defp format_datetime(datetime) do
+    DateTime.shift_zone!(datetime, "Etc/UTC")
+    |> DateTime.truncate(:second)
+    |> DateTime.to_iso8601(:basic)
+  end
 end
